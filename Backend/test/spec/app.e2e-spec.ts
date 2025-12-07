4bbc236e82526307a9df57a3b569ddff36eb5513
イアン <janivantot@gmail.com>
2025-12-07 17:46:00 +0800

    Lint Checks
    Testing Lint Capabilities

--- Backend/test/spec/app.e2e-spec.ts
+++ Backend/test/spec/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Server } from 'http';
 import request from 'supertest';
 import { AppModule } from '../../src/app.module';
 
@@ -16,9 +17,7 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    const httpServer = app.getHttpServer() as Server;
+    return request(httpServer).get('/').expect(200).expect('Hello World!');
   });
 });

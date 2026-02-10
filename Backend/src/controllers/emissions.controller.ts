3c13f0f26719f649762acbe4444e582d56aa2a38
イアン <janivantot@gmail.com>
2026-02-11 01:17:24 +0800

    Detailed Emissions
    Added Modal for Emissions History

--- Backend/src/controllers/emissions.controller.ts
+++ Backend/src/controllers/emissions.controller.ts
@@ -1,4 +1,9 @@
-import { Controller, Get, InternalServerErrorException } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Param,
+} from '@nestjs/common';
 import { DatabaseService } from '../service/database/database.service';
 
 @Controller('emissions')
@@ -41,6 +46,10 @@ export class EmissionsController {
       const result = await client.query(`
         SELECT DISTINCT ON (m.machine_id)
           m.machine_id,
+          CASE 
+            WHEN uc.first_name IS NULL AND uc.last_name IS NULL THEN 'No Owner'
+            ELSE TRIM(CONCAT(uc.first_name, ' ', uc.last_name))
+          END as full_name,
           fa.methane,
           fa.hydrogen,
           fa.benzene,
@@ -48,6 +57,8 @@ export class EmissionsController {
           fa.nitrogen,
           fa.date_created as last_reading
         FROM machines m
+        LEFT JOIN machine_customers mc ON m.machine_id = mc.machine_id
+        LEFT JOIN user_customer uc ON mc.customer_id = uc.customer_id
         LEFT JOIN fertilizer_analytics fa ON m.machine_id = fa.machine_id
         ORDER BY m.machine_id, fa.date_created DESC
       `);
@@ -60,6 +71,39 @@ export class EmissionsController {
       console.error('Device Emissions Error:', error);
       throw new InternalServerErrorException(
         'Failed to fetch device emissions',
+      );
+    }
+  }
+
+  @Get('history/:machineId')
+  async getMachineEmissionsHistory(@Param('machineId') machineId: string) {
+    const client = this.databaseService.getClient();
+    try {
+      const result = await client.query(
+        `
+        SELECT 
+          nitrogen,
+          methane,
+          hydrogen,
+          smoke,
+          benzene,
+          date_created
+        FROM fertilizer_analytics
+        WHERE machine_id = $1
+        ORDER BY date_created DESC
+        LIMIT 50
+      `,
+        [machineId],
+      );
+
+      return {
+        ok: true,
+        history: result.rows,
+      };
+    } catch (error) {
+      console.error('Machine Emissions History Error:', error);
+      throw new InternalServerErrorException(
+        'Failed to fetch machine emissions history',
       );
     }
   }

17081b2750df69f2e569594544531c80a9607842
イアン <janivantot@gmail.com>
2026-03-06 11:48:06 +0800

    Change Interval to 1 sec exec, 5 sec interval
    Checking and Executing

--- Backend/src/service/scheduler/machines-monitor.service.ts
+++ Backend/src/service/scheduler/machines-monitor.service.ts
@@ -14,7 +14,7 @@ export class MachinesMonitorService {
   }
 
   // Run every 1 second and mark machines inactive if no data for 20s
-  @Interval(5000)
+  @Interval(1000)
   async checkMachines() {
     const client = this.databaseService.getClient();
     if (!client) {
